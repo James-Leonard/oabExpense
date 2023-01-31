5ed634b0626903f5e016118ffcde63081e884a0d
james-leonard <merrymerry1320@gmail.com>
2023-01-31 18:19:03 +0100

    added trusted host:

--- core/settings.py
+++ core/settings.py
@@ -28,9 +28,9 @@ DEBUG = env('DEBUG')
 ASSETS_ROOT = os.getenv('ASSETS_ROOT', '/static/assets')
 
 # load production server from .env
-ALLOWED_HOSTS = ['*', 'localhost', 'localhost:85', '127.0.0.1',
+ALLOWED_HOSTS = ['*', 'localhost', 'localhost:85', 'https://oabexpense-production.up.railway.app', '127.0.0.1',
                  env('SERVER', default='127.0.0.1')]
-CSRF_TRUSTED_ORIGINS = ['http://localhost:85', 'http://127.0.0.1',
+CSRF_TRUSTED_ORIGINS = ['http://localhost:85', 'https://oabexpense-production.up.railway.app', 'http://127.0.0.1',
                         'https://' + env('SERVER', default='127.0.0.1')]
 
 # Application definition
