37ee19915e8cebff67c6b04fb675114c946c25ab
james-leonard <merrymerry1320@gmail.com>
2023-02-04 02:57:01 +0100

    removed search feature

--- apps/expense/urls.py
+++ apps/expense/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('add_expense', views.add_expense, name="add_expense"),
     path('edit-expense/<int:id>', views.expense_edit, name="expense-edit"),
     path('expense-delete/<int:id>', views.delete_expense, name="expense-delete"),
-    path('search-expenses/', csrf_exempt(views.search_expenses),
-         name="search_expenses"),
+    # path('search-expenses/', csrf_exempt(views.search_expenses),
+    #      name="search_expenses"),
 
 ]
--- apps/expense/views.py
+++ apps/expense/views.py
@@ -114,13 +114,13 @@ def delete_expense(request, id):
 #             category__icontains=search_str, owner=request.user)
 #         data = expenses.value()
 #         return JsonResponse(list(data), safe=False)
-def search_expenses(request):
-    if request.method == 'POST':
-        search_str = json.loads(request.body).get('searchText')
-        expenses = Expense.objects.filter(
-            amount__istartswith=search_str, owner=request.user) | Expense.objects.filter(
-            date__istartswith=search_str, owner=request.user) | Expense.objects.filter(
-            description__icontains=search_str, owner=request.user) | Expense.objects.filter(
-            category__icontains=search_str, owner=request.user)
-        data = expenses.values()
-        return JsonResponse(list(data), safe=False)
+# def search_expenses(request):
+#     if request.method == 'POST':
+#         search_str = json.loads(request.body).get('searchText')
+#         expenses = Expense.objects.filter(
+#             amount__istartswith=search_str, owner=request.user) | Expense.objects.filter(
+#             date__istartswith=search_str, owner=request.user) | Expense.objects.filter(
+#             description__icontains=search_str, owner=request.user) | Expense.objects.filter(
+#             category__icontains=search_str, owner=request.user)
+#         data = expenses.values()
+#         return JsonResponse(list(data), safe=False)
