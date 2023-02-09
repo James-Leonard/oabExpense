125b020a072a7d6546f8623c55b2e4ca24f561e0
Chiagoziem James <66981472+James-Leonard@users.noreply.github.com>
2023-02-09 03:10:36 +0100

    Added try and except on expense view

--- apps/expense/views.py
+++ apps/expense/views.py
@@ -28,7 +28,10 @@ def expense(request):
     paginator = Paginator(expenses, 5)
     page_number = request.GET.get('page')
     page_obj = Paginator.get_page(paginator, page_number)
-    currency = UserPreference.objects.get(user=request.user).currency
+    try:
+        currency = UserPreference.objects.get(user=request.user).currency 
+    except UserPreference.DoesNotExist:
+        currency = None 
 
     context = {
         'expenses': expenses,
